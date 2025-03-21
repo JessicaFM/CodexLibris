d8e9a80595fdeb3da61cdaeccaacf3663bf60d4b
Jessica F Martinez <jessicafndez@gmail.com>
2025-03-21 20:55:15 +0100

    ADD Open Library

---
+++ API/CodexLibrisAPI/src/main/java/com/codexlibris/service/OpenLibraryClient.java
@@ -0,0 +1,32 @@
+package com.codexlibris.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import com.codexlibris.dto.BookSearchResponseDTO;
+
+/**
+ *
+ * @author jessica
+ */
+@Service
+public class OpenLibraryClient {
+
+    private final WebClient webClient;
+
+    public OpenLibraryClient(WebClient.Builder builder) {
+        this.webClient = builder.baseUrl("https://openlibrary.org").build();
+    }
+
+    public BookSearchResponseDTO searchBooks(String query) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/search.json")
+                        .queryParam("q", query)
+                        .queryParam("language", "spa")
+                        .build())
+                .retrieve()
+                .bodyToMono(BookSearchResponseDTO.class)
+                .block();
+    }
+}
